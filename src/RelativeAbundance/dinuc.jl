a180d53ba2139f5291bb6cb0a5774bacb2172245
Nicolas Palopoli <nicopalo@gmail.com>
2015-08-18 01:07:39 -0300

    [WIP] calculate multiple kmer count
    Call nucfreq from nucfreqfromfile with [1,2,3,4] and store all kmers in
    same dictionary.

--- src/RelativeAbundance/dinuc.jl
+++ src/RelativeAbundance/dinuc.jl
@@ -1,46 +1,74 @@
 Pkg.add("FastaIO")
+Pkg.update()
+Pkg.status()
+pwd()
+cd("C:\\Users\\Nico\\Documents\\UFQ-FIL\\CompositionalAnalysis.jl\\src\\RelativeAbundance")
 
 using FastaIO
 
-function dinuc_freq(seq::ASCIIString)
-  N = length(seq) - 1
-  counter = Dict{ASCIIString, Float64}()
+function nucfreq(seq::ASCIIString,k::Int)
+  N = length(seq) - k + 1
+  countdinuc = Dict{ASCIIString, Float64}()
   for i in 1:N
-    dinuc = seq[i:i+1]
-    counter[dinuc] = get(counter, dinuc, 0.0) + 1/N
+    dinuc = seq[i:i+k-1]
+    countdinuc[dinuc] = get(countdinuc, dinuc, 0.0) + 1/N
   end
-  counter
+  countdinuc
 end
 
-function dinucfreqfromfile(file::ASCIIString)
-  list = Dict{ASCIIString, Dict{ASCIIString, Float64}}()
+function nucfreqfromfile(file::ASCIIString)
+  listnuc = Dict{ASCIIString, Dict{ASCIIString, Float64}}()
   FastaReader(file) do fr
     for (id, seq) in fr
-      list[id] = dinuc_freq(seq)
+#      listnuc[id] = nucfreq(seq,2)
+      for i in [1,2]
+        listnuc[id] = nucfreq(seq,i)
+#        listdinuc[id] = push!(get(listdinuc, id, 0),nucfreq(seq,i))
+#        listnuc[id] = nucfreq(seq,i)
+      end
+#      listnuc[id] = map(nucfreq,(seq,(1:2)))
+#      listnuc[id] = map(2) do x; nucfreq(seq,x); end
+#      listnuc[id] = x -> nucfreq(seq,x), [1,2,3,4]
     end
   end
-  list
+  listnuc
 end
 
-test = dinucfreqfromfile("test.fasta")
+listdinuc = nucfreqfromfile("test.fasta")
 
-using Base.Test
 
-for freq in values(test)
-  @test_approx_eq sum(values(freq)) 1.0
+map([A, B, C]) do x
+    if x < 0 && iseven(x)
+        return 0
+    elseif x == 0
+        return 1
+    else
+        return x
+    end
 end
 
-Pkg.add("BioSeq")
 
-using BioSeq
+# fib(n) = n < 2 ? n : fib(n - 1) + fib(n - 2)
+# fib(6)
 
-ntr"ACN"
 
-nt"ACN"
+# using Base.Test
 
-macro nico_str(str)
-  println("Nico dice: $str")
-end
+# for freq in values(test)
+#   @test_approx_eq sum(values(freq)) 1.0
+# end
 
-nico"Hola"
-@nico_str "hola"
+# Pkg.add("BioSeq")
+
+# using BioSeq
+
+# ntr"ACN"
+
+# nt"ACN"
+
+# macro nico_str(str)
+#   println("Nico dice: $str")
+# end
+
+# nico"Hola"
+# @nico_str "hola"
